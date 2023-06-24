3b7e7924dedd83272af6a54e25a854d6b7191baf
yvijayk <126379757+yvijayk@users.noreply.github.com>
2023-06-24 07:29:11 +0530

    initilal

---
+++ src/main/java/com/test/Login.java
@@ -0,0 +1,24 @@
+package com.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("login service");
+		PrintWriter pw = response.getWriter();
+
+		pw.println("Welcome to CICI pipe line");
+
+		pw.close();
+	}
+
+}
